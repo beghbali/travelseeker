6b8c32a5ae9ef8f3bad77d9ecfd39f9d6de122e3
Bashir Eghbali <bashir@gmail.com>
2015-02-13 03:27:28 -0800

    send to support

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -1,9 +1,9 @@
 class RequestMailer < ActionMailer::Base
-  default from: 'support@zentrips.co'
 
   def new_request(params)
     mail(
-      to: %w(bashir@gmail.com everitt.rory@gmail.com),
+      from: 'support@zentrips.co',
+      to: 'support@zentrips.co',
       subject: "Zentrip [new-request]: #{params[:name]}, #{params[:destination]}, #{params[:arrival]}, #{params[:budget]}",
       body: params.map{|k,v| "#{k}: #{v}"}.join("\n"))
   end
