c8a46e6e0b1e5083cf56f346380a8469056f0aea
Bashir Eghbali <bashir@gmail.com>
2015-02-13 02:50:51 -0800

    make mailer work with ses

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -1,7 +1,9 @@
 class RequestMailer < ActionMailer::Base
+  default from: 'support@zentrips.co'
 
   def new_request(params)
-    mail(to: %w(bashir@gmail.com everitt.rory@gmail.com),
+    mail(
+      to: %w(bashir@gmail.com everitt.rory@gmail.com),
       subject: "Zentrip [new-request]: #{params[:name]}, #{params[:destination]}, #{params[:arrival]}, #{params[:budget]}",
       body: params.map{|k,v| "#{k}: #{v}"}.join("\n"))
   end
