97114c77533d0993da4ee5996e55fb5033467a5e
Bashir Eghbali <bashir@gmail.com>
2015-02-13 19:09:32 -0800

    add mailer back

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -7,7 +7,7 @@ class RequestsController < ApplicationController
 
   def create
     params.require(:request).permit(user_attributes: [:first_name, :email])
-    # RequestMailer.new_request(params).deliver
+    RequestMailer.new_request(params).deliver
     redirect_to thank_you_requests_path
   end
 
