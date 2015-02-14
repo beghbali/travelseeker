ed8779e647f60b8697918446c60d3a407816c366
Bashir Eghbali <bashir@gmail.com>
2015-02-13 19:14:09 -0800

    fix up strong params and email

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -6,8 +6,8 @@ class RequestsController < ApplicationController
   end
 
   def create
-    params.require(:request).permit(user_attributes: [:first_name, :email])
-    RequestMailer.new_request(params).deliver
+    params.require(:request).permit!
+    RequestMailer.new_request(params[:request]).deliver
     redirect_to thank_you_requests_path
   end
 
