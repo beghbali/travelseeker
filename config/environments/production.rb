ed911a0d03e250203da5423dacf5b3d5af9bf1a2
Bashir Eghbali <bashir@gmail.com>
2015-02-13 19:25:57 -0800

    revert to ses

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Zentrips::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
@@ -78,13 +78,13 @@ Zentrips::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address => 'email-smtp.us-west-2.amazonaws.com',
-      :authentication => :login,
-      :user_name => ENV['SMTP_USERNAME'],
-      :password => ENV['SMTP_PASSWORD'],
-      :enable_starttls_auto => true,
-      :port => 465
-    }
+  config.action_mailer.delivery_method = :ses
+  # config.action_mailer.smtp_settings = {
+  #     :address => 'email-smtp.us-west-2.amazonaws.com',
+  #     :authentication => :login,
+  #     :user_name => ENV['SMTP_USERNAME'],
+  #     :password => ENV['SMTP_PASSWORD'],
+  #     :enable_starttls_auto => true,
+  #     :port => 465
+  #   }
 end
