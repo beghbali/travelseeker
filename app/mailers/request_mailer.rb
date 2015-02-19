00555fc28b027688247c9d5b6325b8a6bbecfaa8
Bashir Eghbali <bashir@gmail.com>
2015-02-19 09:25:45 -0800

    use html mail template

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -3,6 +3,7 @@ class RequestMailer < ActionMailer::Base
   default from: 'support@zentrips.co'
 
   def new_request(params)
+    @params = params
     mail(
       to: 'support@zentrips.co',
       subject: "Zentrip [new-request]: #{params[:name]}, #{params[:destination]}, #{params[:arrival]}, #{params[:budget]}")
