1a4e634245fa5ac6d795c7a96fb871205e28fded
Bashir Eghbali <bashir@gmail.com>
2015-03-29 08:40:54 -0700

    reference asset pipeline

--- app/helpers/request_helper.rb
+++ app/helpers/request_helper.rb
@@ -3,7 +3,7 @@ module RequestHelper
   def background_image_for(destination)
     image_name = "#{destination.try(:downcase)}.jpg"
     if Rails.application.assets.find_asset(image_name)
-      "background-image: url(/assets/#{image_name})"
+      "background-image: url(#{asset_path(image_name)})"
     else
       ""
     end
