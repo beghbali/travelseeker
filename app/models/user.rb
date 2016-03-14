fe4edbd9ac92a21402d254e2a4aea8d10b986b54
Bashir Eghbali <bashir@gmail.com>
2016-03-14 09:49:08 -0700

    fix mytrips showing non top level trips

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :trips
+  has_many :trips, -> { top_level }
 
   def claim_trips_for_session!(session_id)
     Trip.where(session_id: session_id).where('user_id is NULL').update_all(user_id: self.id)
