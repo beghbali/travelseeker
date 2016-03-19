153f7dde374b7e4b3c608d7f940996bb512d6f55
Bashir Eghbali <bashir@gmail.com>
2016-03-19 12:39:23 -0700

    allow accounts to be created with nil email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :trips, -> { top_level }
 
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, allow_nil: true
 
   def claim_trips_for_session!(session_id)
     Trip.where(session_id: session_id).where('user_id is NULL').update_all(user_id: self.id)
