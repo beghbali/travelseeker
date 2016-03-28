bc02ac93eee5bd3b52b70cfdee7e639fa69bce60
Bashir Eghbali <bashir@gmail.com>
2016-03-28 09:42:28 -0700

    claim all trips even if previously claimed by session id

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true, allow_nil: true
 
   def claim_trips_for_session!(session_id)
-    Trip.where(session_id: session_id).where('user_id is NULL').update_all(user_id: self.id)
+    Trip.where(session_id: session_id).update_all(user_id: self.id)
   end
 end
