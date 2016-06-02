cab6124018dedda825ff4ec8ed5013df19c43eaf
Bashir Eghbali <bashir@gmail.com>
2016-06-02 09:04:04 -0700

    redirect to trip edit view after login

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -16,7 +16,7 @@ class TripsController < ApplicationController
   # GET /trips/1.json
   def show
     @select_all = true
-    session[:redirect_to] = trip_share_url(@trip) if @trip.readonly?
+    session[:redirect_to] = @trip.readonly? ? trip_share_url(@trip) : trip_edit_url(@trip)
     redirect_to trip_day_path(@trip.presentable_id, day: 1) unless @trip.readonly? || @day.present? || performed?
   end
 
